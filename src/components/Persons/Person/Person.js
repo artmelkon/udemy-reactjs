498c7ed3225959deb07e150d958751cc98ca6a86
Arthur Melkonyan <45238571+artmelkon@users.noreply.github.com>
2019-04-28 20:57:28 -0700

    update 042819

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
 import Auxiliary from '../../../hoc/Auxiliary';
+import PropTypes from 'prop-types';
 
 class Person extends Component {
     render() {
@@ -17,6 +18,13 @@ class Person extends Component {
             </Auxiliary>
         );
     }
+};
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
